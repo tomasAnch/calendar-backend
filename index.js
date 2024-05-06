38a29387d3dc570198c62dcfbb9be8c342f74d45
Tomas Anchorena <tomas_2806@hotmail.com>
2024-05-06 16:52:17 -0300

    rutas y status

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+
+// Crear el servidor de express
+const app = express();
+
+// Directorio público
+app.use( express.static('public') );
+
+//Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD: Eventos
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log( `Servidor corriendo en el puerto ${ process.env.PORT }` );
+});
