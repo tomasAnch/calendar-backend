49048677c998148c1a87f8b8ad7e535bc1c2ff5c
Tomas2806 <tomas_2806@hotmail.com>
2024-05-08 16:12:33 -0300

    custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,8 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body;
-
-    // Manejo de Errores
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
     res.status(201).json({
         ok: true,
@@ -27,15 +17,6 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body;
-
-    // Manejo de Errores
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
     res.status(201).json({
         ok: true,
@@ -58,3 +39,6 @@ module.exports = {
     loginUsuario,
     revalidarToken,
 }
+
+// tomas2806
+// 6HtI9vSEaUh0bOZ7
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const router = Router();
-
+const { validarCampos } = require('../middlewares/validarCampos');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
 
 router.post(
     '/new',
@@ -16,6 +17,7 @@ router.post(
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('password', 'El password debe ser de 6 caracteres o más').isLength({ min: 6 }),
+        validarCampos
     ],
     crearUsuario);
 
@@ -25,6 +27,7 @@ router.post(
     [
         check('email', 'El email es inválido').isEmail(),
         check('password', 'El password debe ser de 6 caracteres o más').isLength({ min: 6 }),
+        validarCampos
     ], 
     loginUsuario);
 
